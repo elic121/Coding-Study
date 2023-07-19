9a9d63d7fe6ad8f28085b6e706c576b453333250
elic121 <elic121@ajou.ac.kr>
2023-07-19 14:11:24 +0900

    .

---
+++ baekjoon/11725_트리의 부모 찾기.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/11725
+
+import sys
+from collections import defaultdict
+sys.setrecursionlimit(10**6)
+
+s = sys.stdin.readline
+N = int(s())
+d = defaultdict(list)
+visited = [0 for _ in range(N+1)]
+
+for _ in range(N-1):
+    a,b = map(int,s().split())
+    d[a].append(b)
+    d[b].append(a)
+
+answer = defaultdict(int)
+
+def dfs(start_node):
+    for i in d[start_node]:
+        if not visited[i]:
+            visited[i] = 1
+            answer[i] = start_node
+            dfs(i)
+
+dfs(1)
+
+for i in range(2,N+1):
+    print(answer[i])
--- baekjoon/7569_토마토.py
+++ baekjoon/7569_토마토.py
@@ -1,18 +1,55 @@
 # https://www.acmicpc.net/problem/7569
 import sys
+from collections import deque
 s = sys.stdin.readline
 M, N, H = map(int, s().split())
 arr = [[list(map(int, s().split())) for _ in range(N)] \
        for _ in range(H)]
+
+
+ripe = deque()
+for k in range(H):
+    for j in range(N):
+        for i in range(M):
+            if arr[k][j][i] == 1:
+                ripe.append((k,j,i))
+
 dx = [0,0,1,-1,0,0]
 dy = [0,0,0,0,1,-1]
 dz = [-1,1,0,0,0,0]
 
-def dfs(i,j,k):
-    return 
+def find_zero(arr):
+    for k in range(H):
+        for j in range(N):
+            for i in range(M):
+                if arr[k][j][i] == 0:
+                    return False
+    return True
 
-for i in range(H):
-    for j in range(N):
-        for k in range(M):
-            if arr[i][j][k] == 1:
-                dfs(i,j,k)
+def bfs():
+    cnt = 0
+    while ripe:
+        if find_zero(arr):
+            return cnt
+        L = len(ripe)
+        for _ in range(L):
+            z,y,x = ripe.popleft()
+            for X,Y,Z in zip(dx,dy,dz):
+                nx, ny, nz = x+X, y+Y, z+Z
+                if (nx<0 or nx>=M) or (ny<0 or ny>=N) or (nz<0 or nz>=H):
+                    continue
+                if arr[nz][ny][nx] != 0:
+                    continue
+                arr[nz][ny][nx] = arr[z][y][x] + 1
+                ripe.append((nz,ny,nx))
+        cnt+=1
+    
+    for i in range(H):
+        for j in range(N):
+            for k in range(M):
+                if arr[i][j][k] == 0:
+                    return -1
+                
+    return cnt
+
+print(bfs())
