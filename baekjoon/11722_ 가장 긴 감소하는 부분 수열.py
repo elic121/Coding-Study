981d99d648001adf8e1442fb2f506c0b9fe2a3cb
elic121 <elic121@ajou.ac.kr>
2023-09-02 16:27:08 +0900

    .

---
+++ baekjoon/11722_ 가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/11722
+from sys import stdin
+s = stdin.readline
+
+N = int(s())
+arr = list(map(int,s().split()))
+L = [1 for _ in range(N)]
+for i in range(N):
+    for j in range(i):
+        if arr[j] > arr[i]:
+            L[i] = max(L[i],L[j]+1)
+
+print(max(L))
