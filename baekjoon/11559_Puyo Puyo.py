4a168a34515d6a6cc32bd05610ee61e14265e9b1
unknown <elic121@ajou.ac.kr>
2023-10-22 14:30:45 +0900

    .

---
+++ baekjoon/11559_Puyo Puyo.py
@@ -0,0 +1,94 @@
+# https://www.acmicpc.net/problem/11559
+from sys import stdin
+from collections import deque
+
+s = stdin.readline
+
+visited = [[0 for _ in range(6)] for _ in range(12)]
+
+
+def init_visited():
+    for i in range(12):
+        for j in range(6):
+            visited[i][j] = 0
+
+
+def condition(x, y):
+    if (x < 0 or x >= 12) or (y < 0 or y >= 6):
+        return False
+    return True
+
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+
+def bfs(i, j):
+    d = deque()
+    d.append((i, j))
+    value = arr[i][j]
+
+    tmp = [(i, j)]
+    while d:
+        x, y = d.popleft()
+        for X, Y in zip(dx, dy):
+            nx, ny = x + X, y + Y
+            if not condition(nx, ny):
+                continue
+            if arr[nx][ny] != value:
+                continue
+            if visited[nx][ny]:
+                continue
+            visited[nx][ny] = 1
+            tmp.append((nx, ny))
+            d.append((nx, ny))
+
+    return [None, tmp][len(tmp) >= 4]
+
+
+def delete(exp):
+    for val in exp:
+        for i, j in val:
+            arr[i][j] = '.'
+
+
+def clean():
+    for c in range(6):
+        stack = []
+        for r in range(11, -1, -1):
+            if arr[r][c] != '.':
+                stack.append(arr[r][c])
+                arr[r][c] = '.'
+        for r in range(11, 11 - len(stack), -1):
+            arr[r][c] = stack[11 - r]
+
+
+def explore():
+    EX = []
+    init_visited()
+    for i in range(12):
+        for j in range(6):
+            if visited[i][j]:
+                continue
+            if arr[i][j] == '.':
+                continue
+            visited[i][j] = 1
+            ex = bfs(i, j)
+            if ex:
+                EX.append(ex)
+
+    if not EX:
+        return 0
+
+    else:
+        delete(EX)
+        clean()
+        return 1
+
+
+if __name__ == "__main__":
+    arr = [list(s().strip()) for _ in range(12)]
+    cnt = 0
+    while explore():
+        cnt += 1
+    print(cnt)
---
+++ baekjoon/14442_벽 부수고 이동하기 2.py
@@ -0,0 +1,48 @@
+# https://www.acmicpc.net/problem/14442
+from sys import stdin
+from collections import deque
+
+s = stdin.readline
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+
+def condition(x, y):
+    if (x < 0 or x >= N) or (y < 0 or y >= M):
+        return False
+    return True
+
+
+def bfs():
+    visited = [[[0]*(K + 1) for _ in range(M)] for _ in range(N)]
+    d = deque()
+    d.append((0, 0, K))
+    visited[0][0][K] = 1
+    while d:
+        x, y, cnt = d.popleft()
+        if x == N - 1 and y == M - 1:
+            return visited[x][y][cnt]
+
+        for X, Y in zip(dx, dy):
+            nx, ny = x + X, y + Y
+            if not condition(nx, ny):
+                continue
+            if arr[nx][ny] == 0:
+                if visited[nx][ny][cnt] >= 1:
+                    continue
+                d.append((nx, ny, cnt))
+                visited[nx][ny][cnt] = visited[x][y][cnt] + 1
+            if arr[nx][ny] == 1 and cnt >= 1:
+                if visited[nx][ny][cnt-1] >= 1:
+                    continue
+                d.append((nx, ny, cnt - 1))
+                visited[nx][ny][cnt - 1] = visited[x][y][cnt] + 1
+
+    return -1
+
+
+if __name__ == "__main__":
+    N, M, K = map(int, s().split())
+    arr = [list(map(int, list(s().strip()))) for _ in range(N)]
+    print(bfs())
