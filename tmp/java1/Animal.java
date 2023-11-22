cc7d5808456057f3735eb9e7536a9d574b0f2839
elic121 <elic121@ajou.ac.kr>
2023-11-22 12:56:24 +0900

    .

---
+++ tmp/java1/Animal.java
@@ -0,0 +1,18 @@
+package java1;
+
+public class Animal {
+
+  String name;
+
+  public void setName(String n) {
+    this.name = n;
+  }
+
+  public void getName() {
+    System.out.println("The name is " + name);
+  }
+
+  public void bark() {
+    System.out.println("hi");
+  }
+}
---
+++ tmp/java1/javaPractice1.java
@@ -0,0 +1,14 @@
+package java1;
+
+
+public class javaPractice1{
+
+    public static void main(String[] args) {
+        Animal cat = new Animal();
+        cat.setName("mweo");
+        cat.getName();    
+        cat.bark();
+    }
+}
+
+    
