261abe8e6eb82bdde5f3d58039b79c58b1e413b7
elic121 <elic121@ajou.ac.kr>
2023-12-26 15:03:09 +0900

    feat: add .gitignore

--- tmp/java1/Animal.java
+++ tmp/java1/Animal.java
@@ -1,5 +1,3 @@
-package java1;
-
 public class Animal {
 
   String name;
--- tmp/java1/javaPractice1.java
+++ tmp/java1/javaPractice1.java
@@ -1,14 +1,9 @@
-package java1;
+public class javaPractice1 {
 
-
-public class javaPractice1{
-
-    public static void main(String[] args) {
-        Animal cat = new Animal();
-        cat.setName("mweo");
-        cat.getName();    
-        cat.bark();
-    }
+  public static void main(String[] args) {
+    Animal cat = new Animal();
+    cat.setName("mweo");
+    cat.getName();
+    cat.bark();
+  }
 }
-
-    
